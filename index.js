5f3b534ebac403020d89678651cc8034c500e31c
PdoubleU <piotrwitasik1988@gmail.com>
2021-05-25 11:24:43 +0200

    [task#13]: slide in at scroll

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-let elem = document.querySelector('.secretCode');
-const keySequence = [];
-const secretCode = 'piter';
-
-function getKeySequence(e) {
-    keySequence.push(e.key);
-    keySequence.splice(-secretCode.length - 1, keySequence.length - secretCode.length)
-    elem.innerHTML = keySequence.join('')
-    if (keySequence.join('') === secretCode) {
-        alert('good!');
+// lodash _.debounce:
+function debounce(func, wait = 30, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if(!immediate) func.apply(context, args)
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
     }
 }
 
-window.addEventListener('keypress', getKeySequence);
+let images = document.querySelectorAll('img');
+
+function animateImage(elem) {
+    images.forEach(img => {
+        const slideInAt = (window.scrollY + window.innerHeight) - img.height / 2;
+        const imgBottom = img.offsetTop + img.height;
+        const isHalfHeightImg = slideInAt > img.offsetTop;
+        const isNotScrolledPast = window.scrollY < imgBottom;
+
+        if (isHalfHeightImg && isNotScrolledPast) {
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+        }
+
+    })
+}
+
+window.addEventListener('scroll', debounce(animateImage));

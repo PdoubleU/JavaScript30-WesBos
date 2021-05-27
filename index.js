efb56f15739658d56905f45e05f422542274befe
PdoubleU <piotrwitasik1988@gmail.com>
2021-05-27 11:57:49 +0200

    [play with localStorage]

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-// lodash _.debounce:
-function debounce(func, wait = 30, immediate = true) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if(!immediate) func.apply(context, args)
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
+const addItems = document.querySelector('.addItem')
+const listsItem = document.querySelector('.listsItem');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+
+function handleInput(e) {
+    e.preventDefault()
+    const text = (document.querySelector('[name=addItem]')).value;
+    const item = {
+        text,
+        done: false
     }
+    items.push(item);
+    localStorage.setItem('items', JSON.stringify(items));
+    updateList(items, listsItem);
+    this.reset();
 }
 
-let images = document.querySelectorAll('img');
-
-function animateImage(elem) {
-    images.forEach(img => {
-        const slideInAt = (window.scrollY + window.innerHeight) - img.height / 2;
-        const imgBottom = img.offsetTop + img.height;
-        const isHalfHeightImg = slideInAt > img.offsetTop;
-        const isNotScrolledPast = window.scrollY < imgBottom;
-
-        if (isHalfHeightImg && isNotScrolledPast) {
-            img.classList.add('active');
-        } else {
-            img.classList.remove('active');
+function updateList(items = [], listsItem) {
+    listsItem.innerHTML = items.map((item, i) => {
+        return `
+        <li>
+            <input type="checkbox" data-index=${i} id="item${i}" ${item.done ? "checked" : null}>
+            <label for="item${i}">${item.text}</label>
+        </li>
+        <hr>
+        `
         }
-
-    })
+    ).join('');
 }
 
-window.addEventListener('scroll', debounce(animateImage));
+const toggleDone = (e) => {
+    if(!e.target.matches('input')) return
+    let elem = e.target;
+    let index = elem.dataset.index
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    updateList(items, listsItem);
+}
+updateList(items, listsItem);
+
+
+listsItem.addEventListener('click', toggleDone);
+addItems.addEventListener('submit', handleInput);

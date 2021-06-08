b2cc13b54a1bef9c6aeaf3eb56ee1dad13aa3e05
PdoubleU <piotrwitasik1988@gmail.com>
2021-06-08 10:58:32 +0200

    [task#26]

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed-value');
+const dropBckg = document.querySelector('.dropdownBackground');
+const navElems = document.querySelectorAll('.cool > li');
+const nav = document.querySelector('.top')
 
-navigator.geolocation.watchPosition((data) => {
-  console.log(data);
-  speed.textContent = data.coords.speed;
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-}, (err) => {
-  console.error(err);
-});
+function openDropdown() {
+    triggerClass(this, dropBckg);
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoors = dropdown.getBoundingClientRect();
+    const navCoors = nav.getBoundingClientRect();
+
+    const coors = {
+        width: dropdownCoors.width,
+        height: dropdownCoors.height,
+        top: dropdownCoors.top - navCoors.top,
+        left: dropdownCoors.left - navCoors.left,
+    }
+
+    dropBckg.style.setProperty('width', coors.width + 'px');
+    dropBckg.style.setProperty('height', coors.height + 'px');
+    dropBckg.style.setProperty('top', coors.top + 'px');
+    dropBckg.style.setProperty('left', coors.left + 'px');
+}
+
+function triggerClass(elem, background) {
+    elem.classList.toggle('trigger-enter');
+    setTimeout(() => elem.classList.toggle('trigger-enter-active'), 50);
+    background.classList.toggle('open');
+}
+
+
+function closeDropdown() {
+    triggerClass(this, dropBckg);
+}
+
+navElems.forEach(e => e.addEventListener('mouseenter', openDropdown));
+navElems.forEach(e => e.addEventListener('mouseleave', closeDropdown));
